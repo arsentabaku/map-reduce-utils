ca57cd439d49406ee31bb069111f0d254baf0e46
Arsen Tabaku <arsentabaku2@gmail.com>
2025-06-25 18:58:25 +0200

    Implemented all plain prod functions in prod.ts file and tested them in index.ts file

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,16 @@ import {
   factorial,
   sumMap,
 } from "./sum";
+import { prodInts, prodSquares, prodCubes, prodFactorial } from "./prod";
+
+console.log("\n---Plain sum functions---\n");
 
 console.log("sumInts => ", sumInts(1, 5));
 console.log("sumSquares => ", sumSquares(1, 5));
 console.log("sumCubes => ", sumCubes(1, 5));
 console.log("sumFactorial => ", sumFactorial(1, 5));
 
-console.log("\n---Below are currying functions---\n");
+console.log("\n---Currying sum functions---\n");
 
 const sumInt2 = sumMap((x) => x);
 console.log("sumInt2 => ", sumInt2(1, 5));
@@ -22,3 +25,10 @@ const sumCubes2 = sumMap((x) => x * x * x);
 console.log("sumCubes2 => ", sumCubes2(1, 5));
 const sumFactorial2 = sumMap((x) => factorial(x));
 console.log("sumFactorial2 => ", sumFactorial2(1, 5));
+
+console.log("\n---Plain prod functions---\n");
+
+console.log("prodInts => ", prodInts(1, 5));
+console.log("prodSquares => ", prodSquares(1, 5));
+console.log("prodCubes => ", prodCubes(1, 5));
+console.log("prodFactorial => ", prodFactorial(1, 5));
---
+++ src/prod.ts
@@ -0,0 +1,19 @@
+export function prodInts(a: number, b: number): number {
+  return a > b ? 1 : a * prodInts(a + 1, b);
+}
+
+export function prodSquares(a: number, b: number): number {
+  return a > b ? 1 : Math.pow(a, 2) * prodSquares(a + 1, b);
+}
+
+export function prodCubes(a: number, b: number): number {
+  return a > b ? 1 : Math.pow(a, 3) * prodCubes(a + 1, b);
+}
+
+export function prodFactorial(a: number, b: number): number {
+  return a > b ? 1 : factorial(a) * prodFactorial(a + 1, b);
+}
+
+export function factorial(n: number): number {
+  return n <= 1 ? 1 : n * factorial(n - 1);
+}
