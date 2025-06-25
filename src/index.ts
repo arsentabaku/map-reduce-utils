1fe937f98c1cc23287d18e4468c660d32f0b957e
Arsen Tabaku <arsentabaku2@gmail.com>
2025-06-25 20:13:30 +0200

    Implemented the mapReduce function and tested that with all samples in index.ts file

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ import {
   prodFactorial,
   prodMap,
 } from "./prod";
+import { mapReduce } from "./map-reduce";
 
 console.log("\n---Plain sum functions---\n");
 
@@ -25,9 +26,9 @@ console.log("\n---Currying sum functions---\n");
 
 const sumInt2 = sumMap((x) => x);
 console.log("sumInt2 => ", sumInt2(1, 5));
-const sumSquares2 = sumMap((x) => x * x);
+const sumSquares2 = sumMap((x) => Math.pow(x, 2));
 console.log("sumSquares2 => ", sumSquares2(1, 5));
-const sumCubes2 = sumMap((x) => x * x * x);
+const sumCubes2 = sumMap((x) => Math.pow(x, 3));
 console.log("sumCubes2 => ", sumCubes2(1, 5));
 const sumFactorial2 = sumMap((x) => factorial(x));
 console.log("sumFactorial2 => ", sumFactorial2(1, 5));
@@ -43,9 +44,53 @@ console.log("\n---Currying sum functions---\n");
 
 const prodInt2 = prodMap((x) => x);
 console.log("prodInt2 => ", prodInt2(1, 5));
-const prodSquares2 = prodMap((x) => x * x);
+const prodSquares2 = prodMap((x) => Math.pow(x, 2));
 console.log("prodSquares2 => ", prodSquares2(1, 5));
-const prodCubes2 = prodMap((x) => x * x * x);
+const prodCubes2 = prodMap((x) => Math.pow(x, 3));
 console.log("prodCubes2 => ", prodCubes2(1, 5));
 const prodFactorial2 = prodMap((x) => factorial(x));
 console.log("prodFactorial2 => ", prodFactorial2(1, 5));
+
+console.log("\n---Reduce functions---\n");
+
+const sumInts3 = mapReduce(
+  (x) => x,
+  (a, b) => a + b,
+  0
+);
+console.log("sumInts3 =>", sumInts3(1, 5));
+const sumSquares3 = mapReduce(
+  (x) => Math.pow(x, 2),
+  (a, b) => a + b,
+  0
+);
+console.log("sumSquares3 =>", sumSquares3(1, 5));
+const sumCubes3 = mapReduce(
+  (x) => Math.pow(x, 3),
+  (a, b) => a + b,
+  0
+);
+console.log("sumCubes3 =>", sumCubes3(1, 5));
+const sumFactorial3 = mapReduce(factorial, (a, b) => a + b, 0);
+console.log("sumFactorial3 =>", sumFactorial3(1, 5));
+
+const prodInts3 = mapReduce(
+  (x) => x,
+  (a, b) => a * b,
+  1
+);
+console.log("prodInts3 =>", prodInts3(1, 5));
+const prodSquares3 = mapReduce(
+  (x) => Math.pow(x, 2),
+  (a, b) => a * b,
+  1
+);
+console.log("prodSquares3 =>", prodSquares3(1, 5));
+const prodCubes3 = mapReduce(
+  (x) => Math.pow(x, 3),
+  (a, b) => a * b,
+  1
+);
+console.log("prodCubes3 =>", prodCubes3(1, 5));
+const prodFactorial3 = mapReduce(factorial, (a, b) => a * b, 1);
+console.log("prodFactorial3 =>", prodFactorial3(1, 5));
